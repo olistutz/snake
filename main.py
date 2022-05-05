21fabba06349767499d57a5d0e5ec4e8f996bab7
Oliver Stutz <oliver@olivers-mbp.edu.ds.fhnw.ch>
2022-05-05 11:44:41 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Programmierprojekt Snake
+
+# Einfügen von bestehenden Libarys
+import pygame
+import time
+import random
+
+# Farben definieren
+weiss = (255, 255, 255)
+gelb = (255, 255, 100)
+
+
+pygame.init()
