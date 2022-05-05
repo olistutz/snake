686724b56123f4d98e9e9518d3b10607acdaa044
olistutz <oliver.stutz@bluewin.ch>
2022-05-05 13:32:03 +0200

    text

--- main.py
+++ main.py
@@ -5,11 +5,23 @@ import pygame
 import time
 import random
 
+pygame.init()
+
 # Farben definieren
 weiss = (255, 255, 255)
 gelb = (255, 255, 100)
+schwarz = (0, 0, 0)
+rot = (200, 50, 80)
+gruen = (0, 255, 0)
+blau = (50, 150, 200)
+
+# Displaygrösse definieren
+display_breite = 600
+display_hoehe = 400
+
+dis = pygame.display.set_mode((display_breite, display_hoehe))
+pygame.display.set_caption('Snake Game')
 
 
-pygame.init()
 
-print("test")
+
